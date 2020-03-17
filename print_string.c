132e7b5e494332fba2573e7aefabce7c8b518bc4
JParrales <josedparrales@gmail.com>
2020-03-16 22:56:57 -0500

    print_string.c

---
+++ print_string.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_string - Print string
+ * @list: list.
+ *
+ * Return: String length.
+ */
+
+int print_string(va_list list)
+{
+	char *aux;
+	int i;
+
+	aux = va_arg(list, char*);
+
+	for (i = 0; aux[i] != 0; i++)
+		;
+
+	return (i);
+}
+
