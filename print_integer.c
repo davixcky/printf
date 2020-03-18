474ea1d02a8657b339e0283b56a111733b881745
David Orozco <dvdizcky@gmail.com>
2020-03-17 22:21:48 -0500

    Betty style added

--- print_integer.c
+++ print_integer.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * print_integer - Print a number in base 10
+ * @list: Number to print in base 10
+ *
+ * Return: Length of th numbers in decimal
+ **/
 int print_integer(va_list list)
 {
 	char *p_buff;
